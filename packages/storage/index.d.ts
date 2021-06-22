b2d0b170a296f3146e21be5d2f691287cb92f196
zhengquan <zhengquan@inovance.com>
2021-06-22 11:53:39 +0800

    add storage

---
+++ packages/storage/index.d.ts
@@ -0,0 +1,15 @@
+declare type StorageType = 'localStorage' | 'sessionStorage'
+declare class Store {
+	store: Storage
+	constructor(key: StorageType)
+	get(key: string): any
+	set(key: string, val: any): void
+	has(key: string): boolean
+	remove(key: string): void
+	clear(): void
+	forEach(callback: (key: string, val: any) => void): void
+}
+declare const storage: {
+	local: Store
+	session: Store
+}
